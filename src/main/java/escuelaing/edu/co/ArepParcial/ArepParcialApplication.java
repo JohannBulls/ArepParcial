e531696df418fc73015c78867915b44df5927d84
unknown <johann.amaya@labinfo.is.escuelaing.edu.co>
2024-10-31 13:37:43 -0500

    Inicialaizer

---
+++ src/main/java/escuelaing/edu/co/ArepParcial/ArepParcialApplication.java
@@ -0,0 +1,13 @@
+package escuelaing.edu.co.ArepParcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArepParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArepParcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/escuelaing/edu/co/ArepParcial/ArepParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package escuelaing.edu.co.ArepParcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArepParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
